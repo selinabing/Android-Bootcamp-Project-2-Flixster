8e4798137dbc859ac3f8ca9b5f42bb07b46e1807
Selina Bing <selinabing@fb.com>
2016-06-15 11:57:02 -0700

    My first commit

---
+++ app/src/main/java/com/codepath/flixster/MoviesAdapter.java
@@ -0,0 +1,54 @@
+package com.codepath.flixster;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by selinabing on 6/15/16.
+ */
+public class MoviesAdapter extends ArrayAdapter<Movie> {
+
+    public MoviesAdapter(Context context, ArrayList<Movie> movies) {
+        super(context, R.layout.item_movie, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Get the data item for this position
+        Movie movie = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_movie, parent, false);
+        }
+
+        // Lookup view for data population
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        ImageView ivPoster = (ImageView) convertView.findViewById(R.id.ivPoster);
+
+        // Populate the data into the template view using the data object
+        tvTitle.setText(movie.title);
+        //tvHome.setText(user.hometown);
+
+        Log.d("MoviesAdapter", "Position: " + position);
+        //NOTE: when printing out positions, it wouldn't print out all (180) movies
+        //it only prints whatever is visible on screen, plus some ±1..2..3.. items
+
+
+        String imageUri = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(getContext()).load(imageUri).into(ivPoster);
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
